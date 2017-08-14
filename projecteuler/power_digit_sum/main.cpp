39471c9bfe9bfe79cf5ec34d3052597eb5fd3eee
ting2016 <ting2016>
2017-08-14 16:58:48 -0400

    add sortTest

--- projecteuler/power_digit_sum/main.cpp
+++ projecteuler/power_digit_sum/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
-
+#include<cassert>
+unsigned long long getPowerFor2(int power){
+	assert(power>=1);
+	if(power==1)
+		return 2;
+	return 2*getPowerFor2(power-1);
+}
 int main(){
-
+	int power;
+	std::cout<<"pleast enter the power: ";
+	std::cin>>power;
+	unsigned long long result = getPowerFor2(power);
+	std::cout<<"result is: "<<result<<std::endl;
+	int sum =0;
+	unsigned long long temp = result;
+	while(temp>0){
+		sum+=temp%10;	
+		temp/=10;
+	}
+	std::cout<<"sum is: "<<sum;
+	return 1;
 }
---
+++ sortTest/sorts.h
@@ -0,0 +1,62 @@
+#ifndef SORTS_H
+#define SORTS_H
+#include <list>
+
+namespace ting{
+	namespace sorts{
+		template <class T>
+		void bubbleSort(std::list<T>&);
+		template <class T>
+		void insertSort(std::list<T>&);
+		template <class T>
+		void selectSort(std::list<T>&);
+		template <class T>
+		void mergeSort(std::list<T>&);
+		template <class T>
+		void shellSort(std::list<T>&);
+		template <class T>
+		void quickSort(std::list<T>&);
+		void sortTest();
+	}
+}
+
+template <class T>             
+void ting::sorts::bubbleSort(std::list<T>&){
+}
+
+template <class T>
+void ting::sorts::insertSort(std::list<T>&){
+}
+
+template <class T>
+void ting::sorts::selectSort(std::list<T>&){
+}
+
+template <class T>
+void ting::sorts::mergeSort(std::list<T>&){
+}
+
+template <class T>
+void ting::sorts::shellSort(std::list<T>&){
+}
+
+template <class T>
+void ting::sorts::quickSort(std::list<T>&){
+}
+
+void ting::sorts::sortTest(){
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
