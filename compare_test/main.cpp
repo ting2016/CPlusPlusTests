d190cd3d90c0be75b6e6bba22119a3aa0c7e49ee
ting2016 <ting2016>
2017-09-27 12:25:51 -0400

    finish compare_test compile and run ok

--- compare_test/main.cpp
+++ compare_test/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 class Person{
 private:
 	int age;
@@ -15,9 +16,8 @@ public:
 
 bool operator<(const Person& p1, const Person& p2){
 	if(p1.age==p2.age)
-		return p1.name<p2.name;
-	else
-		return p1.age<p1.age;
+		return (p1.name<p2.name);
+	return (p1.age<p2.age);
 }
 
 std::ostream& operator<<(std::ostream& os, const Person& p){
@@ -47,12 +47,38 @@ void display(const It begin, const It end){
 	std::cout<<std::endl;
 }
 
+template<class It, class Compare>
+void my_sort(It begin, It end, Compare comp){
+
+
+}
+
+template <class It>
+void my_sort(It begin, It end){
+
+}
+
 
 int main(){
+	int a[5]{2,5,1,8,6};
+	std::vector<int> intV(a, a+5);
+	std::sort(intV.begin(), intV.end());
+	std::cout<<"after sort intv:\n";
+	display(intV.cbegin(), intV.cend());
+	
 	std::vector<Person> personVec;
 	personVec.push_back(Person(22,"lifang"));
 	personVec.push_back(Person(37,"zhangting"));
 	personVec.push_back(Person(25,"linlin"));
-	std::sort(personVec.begin(), personVec.end());
+	personVec.push_back(Person(25, "liangliang"));
+	std::cout<<"before sort personVec:\n";
+	display(personVec.cbegin(), personVec.cend());
+	//method1: using compare class to do the comparision
+	std::sort(personVec.begin(), personVec.end(),Compare_class<Person>());
+	//method2: using compare function to do the comparison
+	//std::sort(personVec.begin(), personVec.end(), compare_func<Person>);
+	//mdthod3: using operator '<' as the comparator
+	//std::sort(personVec.begin(), personVec.end());
+	std::cout<<"after sort personVec:\n";
 	display(personVec.cbegin(), personVec.cend());
 }
